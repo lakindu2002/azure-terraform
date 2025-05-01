04306c91cca6e5b5736b7470909ac818461a38f0
lakindu hewawasam <lakinduhewa@gmail.com>
2025-05-01 17:20:25 +0530

    feaT: updating name

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "azure_samples" {
 
 module "storage_account" {
   source                   = "../../modules/storage_account"
-  storage_account_name     = "mystorageaccountwithmodule"
+  storage_account_name     = "lakindusstoragemodule"
   resource_group_name      = azurerm_resource_group.azure_samples.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
