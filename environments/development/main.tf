386e8641b047476cac4d766c80b2826f2b23a77d
lakindu hewawasam <lakinduhewa@gmail.com>
2025-05-01 16:08:08 +0530

    feat: adding the module

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -3,33 +3,21 @@ resource "azurerm_resource_group" "azure_samples" {
   location = "East US"
 }
 
-resource "azurerm_cosmosdb_account" "sample-cosmos-db" {
-  name                = "lakindu-sample-cosmos-db"
-  resource_group_name = azurerm_resource_group.azure_samples.name
-  geo_location {
-    failover_priority = 0
-    location          = "East US"
-    zone_redundant    = true
-  }
-  consistency_policy {
-    consistency_level = "Session"
-  }
-  offer_type                    = "Standard"
-  location                      = "East US"
-  enable_free_tier              = true
-  public_network_access_enabled = true
-}
-
-resource "azurerm_storage_account" "media" {
-  name                     = "lakindu-media"
+module "storage_account" {
+  source                   = "../../modules/storage_account"
+  storage_account_name     = "mystorageaccountwithmodule"
   resource_group_name      = azurerm_resource_group.azure_samples.name
-  account_tier             = "Cool"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
-  location                 = "East US"
-}
-
-resource "azurerm_storage_container" "static" {
-  name                  = "static"
-  storage_account_name  = azurerm_cosmosdb_account.sample-cosmos-db.name
-  container_access_type = "private"
+  location                 = azurerm_resource_group.azure_samples.location
+  containers = [
+    {
+      name        = "container1"
+      access_type = "private"
+    },
+    {
+      name        = "container2"
+      access_type = "blob"
+    }
+  ]
 }
