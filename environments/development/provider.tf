2d23b00e1c8f0d84a26e5806c5af4a652e99c785
lakindu hewawasam <lakinduhewa@gmail.com>
2025-05-25 12:44:40 +0530

    feat: adding azur ebackend

--- environments/development/provider.tf
+++ environments/development/provider.tf
@@ -1,22 +1,18 @@
 terraform {
-  cloud {
-    organization = "blog-writing"
-    workspaces {
-      name = "azure-samples"
-    }
-  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~>3.0"
     }
   }
-}
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "lakindustfstateaccount"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 
+}
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
 }
